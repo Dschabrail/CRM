a645e8c84763114146f0b8de7addf4c5a9fbe601
Dschabrail <141074515+Dschabrail@users.noreply.github.com>
2023-10-11 17:49:40 +0200

    installed firebase again

---
+++ src/environments/environment.ts
@@ -0,0 +1,10 @@
+export const environment = {
+  firebase: {
+    projectId: 'simplecrm-2810e',
+    appId: '1:322021210565:web:defcb3ecabd5dc900973ac',
+    storageBucket: 'simplecrm-2810e.appspot.com',
+    apiKey: 'AIzaSyDmm3aJCi-8hI37VrIBFIVvLFOw08-OQRQ',
+    authDomain: 'simplecrm-2810e.firebaseapp.com',
+    messagingSenderId: '322021210565',
+  },
+};
