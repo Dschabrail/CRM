c3bd49c66cee852594e8bffb9132fc75c0e4caa8
Dschabrail <141074515+Dschabrail@users.noreply.github.com>
2024-01-12 18:57:21 +0100

    currency calculator ts

--- src/app/tools/currency-calculator/currency-calculator.component.ts
+++ src/app/tools/currency-calculator/currency-calculator.component.ts
@@ -3,44 +3,55 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-currency-calculator',
   templateUrl: './currency-calculator.component.html',
-  styleUrls: ['./currency-calculator.component.scss']
+  styleUrls: ['./currency-calculator.component.scss'],
 })
 export class CurrencyCalculatorComponent {
-  currency_one: string = '';
+  currency_one: string = "USD";
   amount_one: number = 1;
-  currency_two: string = '';
+  currency_two: string = "EUR";
   amount_two: number = 0;
   rate: number = 0;
 
   ngOnInit() {
-    this.calculate(); // Initial calculation
-
-    // Event listeners
-    this.currencyEl_one.addEventListener('change', () => this.calculate());
-    this.amountEl_one.addEventListener('input', () => this.calculate());
-    this.currencyEl_two.addEventListener('change', () => this.calculate());
-    this.amountEl_two.addEventListener('input', () => this.calculate());
-
-    this.swap.addEventListener('click', () => {
-      const temp = this.currency_one;
-      this.currency_one = this.currency_two;
-      this.currency_two = temp;
-      this.calculate();
-    });
+    this.calculate();
   }
 
   calculate() {
-    this.currency_one = this.currencyEl_one.value;
-    this.currency_two = this.currencyEl_two.value;
+    const currency_one = this.currencyEl_one.value !== "" ? this.currencyEl_one.value : 'USD';
+    const currency_two = this.currencyEl_two.value !== "" ? this.currencyEl_two.value : 'EUR';
 
-    fetch(`https://api.exchangerate-api.com/v4/latest/${this.currency_one}`)
-      .then(res => res.json())
-      .then(data => {
-        this.rate = data.rates[this.currency_two];
+    fetch(`https://api.exchangerate-api.com/v4/latest/${currency_one}`)
+      .then((res) => res.json())
+      .then((data) => {
+        const rate = data.rates[currency_two];
 
-        this.rateEl.innerText = `1 ${this.currency_one} = ${this.rate} ${this.currency_two}`;
-        this.amount_two = this.amount_one * this.rate;
+        this.rateEl.innerText = `1 ${currency_one} = ${rate} ${currency_two}`;
+
+        this.amount_two = this.amount_one * rate;
       });
+  }
+
+  onCurrencyOneChange() {
+    this.calculate();
+  }
+
+  onAmountOneInput() {
+    this.calculate();
+  }
+
+  onCurrencyTwoChange() {
+    this.calculate();
+  }
+
+  onAmountTwoInput() {
+    this.calculate();
+  }
+
+  onSwapClick() {
+    const temp = this.currency_one;
+    this.currency_one = this.currency_two;
+    this.currency_two = temp;
+    this.calculate();
   }
 
   get currencyEl_one(): HTMLSelectElement {
